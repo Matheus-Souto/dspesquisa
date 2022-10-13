df0bb7bbc912c4f754a355e35b228f0fc75dabe8
Matheus Souto <emaildopaypal007@gmail.com>
2022-10-13 00:24:57 -0300

    DTO, Service

--- backend/src/main/java/com/devsuperior/dspesquisa/entities/Genre.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/entities/Genre.java
@@ -29,21 +29,35 @@ public class Genre implements Serializable {
         return games;
     }
 
+    
+
     public Long getId() {
         return id;
     }
+
+
 
     public void setId(Long id) {
         this.id = id;
     }
 
+
+
     public String getName() {
         return name;
     }
 
+
+
     public void setName(String name) {
         this.name = name;
     }
+
+    public Genre() {
+        
+    }
+
+
 
     public Genre(Long id, String name) {
         super();
